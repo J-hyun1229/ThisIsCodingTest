5536e6220a82220c4436653a77d153539c4aa97e
hdXian <hyun353500@naver.com>
2022-07-27 15:07:57 +0900

    0727 desktop commit

--- PythonCode/Chap8/Codes/ex8_6_ant_warrior.py
+++ PythonCode/Chap8/Codes/ex8_6_ant_warrior.py
@@ -2,24 +2,13 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-# 각 위치부터 방문을 시작했을 때 구할 수 있는 식량의 최댓값 저장
-arr2 = [0] * n
+d = [0] * 101
 
-for i in range(n):
-    # 현재 위치에서 얻을 수 있는 식량
-    curIndex = i
-    arr2[i] = arr[i]
+d[0] = arr[0]
+d[1] = max(arr[0], arr[1])
 
-    maxIndex = 0
-    maxVal = 0
-    for j in range(n):
-        if j == i or abs(i-j) == 1:
-            continue
-        if arr[maxIndex] < arr[j]:
-            maxIndex = j
-            maxVal = max[j]
-
-    arr2[i] += maxVal
+for i in range(2, n):
+    d[i] = max(d[i-1], d[i-2] + arr[i])
 
 
-print(max(arr2))
+print(d[n-1])
